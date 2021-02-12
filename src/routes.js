ef56d7fc77e977459266d8ce784e2cc24749782b
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-02-12 18:21:33 -0300

    Update controllers and validators imports

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,13 @@ import { Router } from 'express';
 
 import DeveloperController from './app/controllers/DeveloperController';
 import DislikeController from './app/controllers/DislikeController';
+import LikeController from './app/controllers/LikeController';
 import MatchController from './app/controllers/MatchController';
-import DeveloperStore from './app/validators/Developer/Store';
 import Authenticate from './app/middlewares/Authenticate';
+import DeveloperValidator from './app/validators/DeveloperValidator';
+import DislikedUserIdValidator from './app/validators/DislikedUserIdValidator';
+import IdValidator from './app/validators/IdValidator';
+import LikedUserIdValidator from './app/validators/LikedUserIdValidator';
 
 const routes = Router();
 
