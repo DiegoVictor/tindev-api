ee473dfb3ac8a24256f03de8047e79b3ce120c52
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-02-12 18:39:16 -0300

    Remove sentry, rate limit and redis

--- src/app.js
+++ src/app.js
@@ -5,10 +5,6 @@ import cors from 'cors';
 import express from 'express';
 import http from 'http';
 import helmet from 'helmet';
-import Sentry from '@sentry/node';
-import RateLimit from 'express-rate-limit';
-import RedisStore from 'rate-limit-redis';
-import redis from 'redis';
 
 import routes from './routes';
 import './database';
@@ -19,31 +15,11 @@ const server = http.Server(app);
 
 setupWebSocket(server);
 
-if (process.env.SENTRY_DSN) {
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-  });
-}
-
 app.use(helmet());
 
 app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV !== 'test') {
-  App.use(
-    new RateLimit({
-      max: 100,
-      windowMs: 1000 * 60 * 15,
-      store: new RedisStore({
-        client: redis.createClient({
-          host: process.env.REDIS_HOST,
-          port: process.env.REDIS_PORT,
-        }),
-      }),
-    })
-  );
-}
 
 app.use('/v1', routes);
 
