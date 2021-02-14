b7ca5ade6f312dd364965134c04faf70208323bb
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-02-14 17:48:01 -0300

    Rename and move factory file

--- tests/utils/factory.js
+++ tests/utils/factory.js
@@ -4,10 +4,10 @@ import faker from 'faker';
 import Developer from '../../src/app/models/Developer';
 
 factory.define('Developer', Developer, {
-  name: faker.name.firstName(),
-  user: faker.internet.userName(),
-  bio: faker.lorem.paragraph(),
-  avatar: faker.image.imageUrl(),
+  name: faker.name.firstName,
+  user: faker.internet.userName,
+  bio: faker.lorem.paragraph,
+  avatar: faker.image.imageUrl,
   likes: [],
   dislikes: [],
 });
