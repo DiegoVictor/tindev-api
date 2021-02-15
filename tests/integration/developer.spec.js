62aeb6c8b20cbc8e4639ab11bd0c4e2d87e16de1
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-02-15 11:03:40 -0300

    Test developer endpoint

--- tests/integration/developer.spec.js
+++ tests/integration/developer.spec.js
@@ -1,38 +1,45 @@
 import request from 'supertest';
 import MockAdapter from 'axios-mock-adapter';
-import Mongoose from 'mongoose';
+import mongoose from 'mongoose';
+import faker from 'faker';
 
 import app from '../../src/app';
-import factory from '../utils/factories';
+import factory from '../utils/factory';
 import Developer from '../../src/app/models/Developer';
-import Api from '../../src/app/services/Api';
+import { api } from '../../src/app/services/GithubUser';
 import jwtoken from '../utils/jwtoken';
 
-const api_mock = new MockAdapter(Api);
-
 describe('Developer', () => {
+  const apiMock = new MockAdapter(api);
+  const url = `http://127.0.0.1:${process.env.APP_PORT}/v1/developers`;
+
   beforeEach(async () => {
     await Developer.deleteMany();
   });
 
   afterAll(async () => {
-    await Mongoose.disconnect();
+    await mongoose.disconnect();
   });
 
   it('should be able to get a list of developers', async () => {
-    const [user] = await factory.createMany('Developer', 5);
+    const [user, ...developers] = await factory.createMany('Developer', 5);
     const token = jwtoken(user.id);
     const response = await request(app)
-      .get('/developers')
+      .get('/v1/developers')
       .set('Authorization', `Bearer ${token}`)
       .send();
 
     expect(Array.isArray(response.body)).toBe(true);
-    expect(response.body).toContainEqual(
-      expect.objectContaining({
-        _id: expect.any(String),
-      })
-    );
+    developers.forEach(developer => {
+      expect(response.body).toContainEqual({
+        ...developer.toJSON(),
+        _id: developer._id.toString(),
+        createdAt: developer.createdAt.toISOString(),
+        updatedAt: developer.updatedAt.toISOString(),
+        likes: [],
+        url: `${url}/${developer._id}`,
+      });
+    });
   });
 
   it('should not be able to get a list of developers', async () => {
@@ -41,7 +48,7 @@ describe('Developer', () => {
     await user.delete();
 
     const response = await request(app)
-      .get('/developers')
+      .get('/v1/developers')
       .set('Authorization', `Bearer ${token}`)
       .expect(400)
       .send();
@@ -56,25 +63,26 @@ describe('Developer', () => {
     const user = await factory.create('Developer');
     const token = jwtoken(user.id);
     const response = await request(app)
-      .get(`/developers/${user._id}`)
+      .get(`/v1/developers/${user._id}`)
       .set('Authorization', `Bearer ${token}`)
       .send();
 
     expect(response.body).toStrictEqual({
       avatar: user.avatar,
       name: user.name,
+      url: `${url}/${user._id}`,
     });
   });
 
   it('should be able to store a new developer', async () => {
     const { user, name, bio, avatar } = await factory.attrs('Developer');
 
-    api_mock
-      .onGet(`users/${user.toLowerCase()}`)
+    apiMock
+      .onGet(`/users/${user.toLowerCase()}`)
       .reply(200, { name, bio, avatar_url: avatar });
 
     const response = await request(app)
-      .post('/developers')
+      .post('/v1/developers')
       .send({ username: user });
 
     expect(response.body).toMatchObject({
@@ -88,14 +96,27 @@ describe('Developer', () => {
     });
   });
 
-  it('should not be able to store a new developer', async () => {
+  it('should be able to signin with an existing developer', async () => {
+    const { user, name, bio, avatar } = await factory.create('Developer', {
+      user: faker.internet.userName().toLowerCase(),
+    });
+
+    apiMock
+      .onGet(`/users/${user.toLowerCase()}`)
+      .reply(200, { name, bio, avatar_url: avatar });
+
     const response = await request(app)
-      .post('/developers')
-      .send();
+      .post('/v1/developers')
+      .send({ username: user });
 
     expect(response.body).toMatchObject({
-      error: 'Bad Request',
-      message: 'Validation fails',
+      developer: {
+        name,
+        user: user.toLowerCase(),
+        bio,
+        avatar,
+      },
+      token: expect.any(String),
     });
   });
 });
