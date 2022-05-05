3ae07e4604e0d506ebaf09695bbf33e25216a708
Diego Victor <diegovictorgonzaga@gmail.com>
2022-05-05 18:53:29 -0300

    Log OS information

--- tests/setup.js
+++ tests/setup.js
@@ -1,4 +1,9 @@
 const { MongoMemoryServer } = require('mongodb-memory-server');
+const os = require('os');
+
+console.log(os.arch());
+console.log(os.platform());
+console.log(os.version());
 
 module.exports = async () => {
   const mongod = await MongoMemoryServer.create();
